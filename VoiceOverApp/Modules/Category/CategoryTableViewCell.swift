4eb15d112ac7da13c3c849e555e682d104e1965f
Oscar Castillo <goldoac@live.com>
2020-09-16 23:21:01 -0600

    feature/CategoryData Shows Category cells with Category Data and Images

--- VoiceOverApp/Modules/Category/CategoryTableViewCell.swift
+++ VoiceOverApp/Modules/Category/CategoryTableViewCell.swift
@@ -11,4 +11,11 @@ class CategoryTableViewCell: UITableViewCell {
     
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var categoryImageView: UIImageView!
+    
+    func setup(with category: Category) {
+        categoryLabel.text = category.name
+        let imageName = category == .technology ? "Tech": category.name
+        let image = UIImage(named: imageName)
+        categoryImageView.image = image
+    }
 }
--- VoiceOverApp/Modules/Category/NewsCategoriesViewController.swift
+++ VoiceOverApp/Modules/Category/NewsCategoriesViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class NewsCategoriesViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     let cellIdentifier = "CategoryTableViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Categoría"
+        setupTableView()
     }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let category = sender as? Category,
+            let viewController = segue.destination as? HeadLinesNewsViewController else {
+                return
+        }
+        
+        viewController.category = category
+    }
+}
+
+extension NewsCategoriesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        Category.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? CategoryTableViewCell,
+            let category = Category(rawValue: indexPath.row) else {
+                return UITableViewCell()
+        }
+        
+        cell.setup(with: category)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let category = Category(rawValue: indexPath.row) else {
+            return
+        }
+        
+        performSegue(withIdentifier: "showHeadLineNews", sender: category)
+    }
+    
 }
