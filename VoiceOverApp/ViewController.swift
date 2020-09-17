cfe268c83e84fafbbce184097817437fa1f9a0b5
Oscar Castillo <goldoac@live.com>
2020-09-16 21:53:51 -0600

    initial project

---
+++ VoiceOverApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceOverApp
+//
+//  Created by Oscar Castillo on 9/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
