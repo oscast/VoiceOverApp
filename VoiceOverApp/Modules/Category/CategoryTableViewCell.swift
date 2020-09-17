5498c9b600db13da592abe85ac4baa7eed1efcf5
Oscar Castillo <goldoac@live.com>
2020-09-16 22:08:16 -0600

    UI-Category Creates UI for Category

---
+++ VoiceOverApp/Modules/Category/CategoryTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryTableViewCell.swift
+//  VoiceOverApp
+//
+//  Created by Oscar Castillo on 9/14/20.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var categoryImageView: UIImageView!
+}
---
+++ VoiceOverApp/Modules/Category/NewsCategoriesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsCategoriesViewController.swift
+//  VoiceOverApp
+//
+//  Created by Oscar Castillo on 9/14/20.
+//
+
+import UIKit
+
+class NewsCategoriesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellIdentifier = "CategoryTableViewCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
