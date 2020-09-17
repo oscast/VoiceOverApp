394f7a331c51bb64923ebab58a786944f01f252e
Oscar Castillo <goldoac@live.com>
2020-09-17 14:13:16 -0600

    feature/accessibleHeadline makes function to make cell accesible every element o grouping to listen the difference. fixes news text

--- VoiceOverApp/Modules/HeadLine/HeadlLineTableViewCell.swift
+++ VoiceOverApp/Modules/HeadLine/HeadlLineTableViewCell.swift
@@ -29,5 +29,47 @@ class HeadlLineTableViewCell: UITableViewCell {
         favoriteQuantityLabel.text = "\(news.likes)"
         let url = URL(string: news.image)
         newsImageView.sd_setImage(with: url, completed: nil)
+        
+        // Son dos funciones para ver la diferencia entre hacer todo accesible o agrupar la información
+        // Agrupar la información puede hacer la navegación mucho más facil.
+        makeGroupAccessible(with: news)
+    }
+    
+    func makeItAccessible(with news: NewsModel) {
+        newsImageView.isAccessibilityElement = true
+        newsImageView.accessibilityTraits = .image
+        newsImageView.accessibilityLabel = news.accesibilityInfo.imageDescription
+        
+        newsInformationLabel.isAccessibilityElement = true
+        newsInformationLabel.accessibilityTraits = .staticText
+        newsInformationLabel.accessibilityLabel = news.accesibilityInfo.newsTitle
+        
+        dateLabel.isAccessibilityElement = true
+        dateLabel.accessibilityTraits = .staticText
+        dateLabel.accessibilityLabel = news.accesibilityInfo.newsDate
+        
+        favoriteQuantityLabel.isAccessibilityElement = true
+        favoriteQuantityLabel.accessibilityTraits = .staticText
+        favoriteQuantityLabel.accessibilityValue = "Número de personas a las que les gustó esta noticia: \(news.likes)"
+        
+        favoriteImageView.isAccessibilityElement = false
+    }
+    
+    func makeGroupAccessible(with news: NewsModel) {
+        newsImageView.isAccessibilityElement = false
+        dateLabel.isAccessibilityElement = false
+        favoriteQuantityLabel.isAccessibilityElement = false
+        favoriteImageView.isAccessibilityElement = false
+        newsInformationLabel.isAccessibilityElement = false
+        
+        isAccessibilityElement = true
+        accessibilityTraits = .staticText
+        
+        let accessibilityText = news.accesibilityInfo.newsTitle +
+            news.accesibilityInfo.newsDate +
+        "Número de personas a las que les gustó esta noticia: \(news.likes)"
+        
+        accessibilityLabel = accessibilityText
+        accessibilityHint = "Toca dos veces para escuchar esta noticia."
     }
 }
